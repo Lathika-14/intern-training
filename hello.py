229b67fb0ee1eb5ca0eb724fa2c7846b86101e5e
Lathika-14 <lathikasrid2006@gmail.com>
2026-06-08 18:18:30 +0530

    added comments

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-print("HELLO WORLD") 
-age=20
-name="Lathika"
-is_intern=True
-print(age)
+print("HELLO WORLD") #printing hello world
+age=20 #age in int
+name="Lathika" #name in string
+is_intern=True #intern in boolean
+print(age) #printing the variables
 print(name)
 print(is_intern)
