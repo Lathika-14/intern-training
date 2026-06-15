7187e4e05f9b4df679ad2988e3f06b7b7db2430d
Lathika-14 <lathikasrid2006@gmail.com>
2026-06-15 17:38:39 +0530

    files moved to week 1 task

--- hello.py
+++
@@ -1,7 +0,0 @@
-print("HELLO WORLD") #printing hello world
-age=20 #age in int
-name="Lathika" #name in string
-is_intern=True #intern in boolean
-print(age) #printing the variables
-print(name)
-print(is_intern)
